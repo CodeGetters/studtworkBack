b33223060ce560ec3697b6bd942e1dd1582a802c
Johnson Xin <2775145275@qq.com>
2023-09-06 15:24:41 +0800

    feat: :sparkles: add the path alias and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
+import "module-alias/register";
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { AppModule } from "@/modules/app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import type { OpenAPIObject } from "@nestjs/swagger";
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle("studTWork")
+    .setDescription("")
+    .setVersion("1.0.0")
+    .addTag("example")
+    .build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("docs", app, document);
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+
+  await app.listen(3000);
 }
 bootstrap();
